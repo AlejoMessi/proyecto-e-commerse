ce55aeeba3b97649287f6fe8099af97067807fc6
alejo moyano <moyano887@gmail.com>
2023-07-17 12:45:36 -0300

    listo

---
+++ js/stock.js
@@ -0,0 +1,165 @@
+// PRODUCTOS
+const productos = [
+    // Abrigos
+    {
+        id: "Pantalon 01",
+        titulo: "Pantalon 01",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "Pantalon 02",
+        titulo: "Pantalon 02",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "Pantalon 03",
+        titulo: "Pantalon 03",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "Pantalon 04",
+        titulo: "Pantalon 04",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "Pantalon 05",
+        titulo: "Pantalon 05",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas 01",
+        titulo: "Camiseta 01",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas 02",
+        titulo: "Camiseta 02",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas 03",
+        titulo: "Camiseta 03",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas 04",
+        titulo: "Camiseta 04",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas 05",
+        titulo: "Camiseta 05",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas 06",
+        titulo: "Camiseta 06",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "camisetas",
+        titulo: "Camiseta 07",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigos 01",
+        titulo: "Abrigos 01",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Abrigos",
+            id: "Abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigos 02",
+        titulo: "Abrigos 02",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigos 03",
+        titulo: "Abrigos 03",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigos 04",
+        titulo: "Abrigos 04",
+        imagen: "https://dummyimage.com/600x400/000/fff",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+
+];
